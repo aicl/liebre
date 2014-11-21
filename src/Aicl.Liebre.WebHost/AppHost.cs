c83e2c68200b107f550fd511898faedacb2b5949
angelcolmenares <angel.ignacio.colmenares@gmail.com>
2014-11-20 23:41:06 -0500

    razor 6!

--- src/Aicl.Liebre.WebHost/AppHost.cs
+++ src/Aicl.Liebre.WebHost/AppHost.cs
@@ -15,22 +15,12 @@ namespace Aicl.Liebre.WebHost
 		}
 
 		public override void Configure(Container container){
+			Plugins.Add(new CorsFeature());
 			Plugins.Add(new RazorFormat());
-			SetConfig(new HostConfig
-				{
-					GlobalResponseHeaders = {
-						{ "Access-Control-Allow-Origin", "*" },
-						{ "Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS" },
-						{ "Access-Control-Allow-Headers", "Content-Type" },
-					},
-					DefaultContentType = "application/json"
-				});
-
 			var appSettings = new AppSettings();
 			var url = appSettings.Get("MONGOLAB_URI", appSettings.Get("MONGOTEST_URI") );
 			var conventions = new ConventionPack { new IgnoreExtraElementsConvention(true) };
 			ConventionRegistry.Register ("IgnoreExtraElements", conventions, _ => true);
-
 			container.Register<Store> (new Store (url));
 		}
 
