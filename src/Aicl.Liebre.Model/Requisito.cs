482599b1f4d39b9673d08f66acf3dd0262daad88
angelcolmenares <angel.ignacio.colmenares@gmail.com>
2014-12-06 12:59:02 -0500

    Requsito incluye: Decreto, OHSAAS, RUC

--- src/Aicl.Liebre.Model/Requisito.cs
+++ src/Aicl.Liebre.Model/Requisito.cs
@@ -7,12 +7,22 @@ namespace Aicl.Liebre.Model
 	{
 		public Requisito ()
 		{
+			Decreto = new Referencia ();
+			OHSAS = new Referencia ();
+			RUC = new Referencia ();
 		}
 
 		public string Codigo { get; set; }
 		public string Descripcion { get; set; }
+		public Referencia Decreto { get; set; }
+		public Referencia OHSAS { get; set; }
+		public Referencia RUC { get; set; }
 	}
 
+	public class Referencia{
+		public string Numerales {get;set;}
+		public string Titulo {get;set;}
+	}
 
 	[Route("/read/requisito","GET")]
 	public class ReadRequisito:IReturn<ReadRequisitoResponse>
