7944d5edbcb467884941416380f16a7e6810236f
angelcolmenares <angel.ignacio.colmenares@gmail.com>
2014-12-13 18:00:07 -0500

    probando phantomjs com ss

--- src/Aicl.Liebre.ServiceInterface/DiagnosticoInfoService.cs
+++ src/Aicl.Liebre.ServiceInterface/DiagnosticoInfoService.cs
@@ -17,9 +17,12 @@ namespace Aicl.Liebre.ServiceInterface
 		public IHttpResult Get(DiagnosticoInfoPdf request){
 			using (var client = new JsonServiceClient(AppConfig.PhantonjsURL)){
 				try{
-					var httpResponse = client.Get<HttpWebResponse>("/DiagnosticoInfoPdf/Index?id={0}&norma={1}".Fmt(request.Id,request.Norma));
+					var httpResponse = client.Get<HttpWebResponse>("/phn-api/read/diagnosticoinfopdf?Id={0}&Norma={1}".Fmt(request.Id,request.Norma));
 					var responseStream= httpResponse.GetResponseStream();
-					Response.AddHeader("Content-Disposition",httpResponse.Headers["Content-Disposition"]);
+					var cd = new System.Net.Mime.ContentDisposition {
+						Inline=true
+					};
+					Response.AddHeader ("Content-Disposition", cd.ToString ());
 					return new HttpResult(responseStream, httpResponse.ContentType);
 				}
 				catch(Exception e){
